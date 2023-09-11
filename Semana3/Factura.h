67f7427549e91068fafa99b4704361cb4a471284
renatoqc <101827901+renatoqc@users.noreply.github.com>
2023-09-11 01:14:04 -0500

    Update Factura.h

--- Semana3/Factura.h
+++ Semana3/Factura.h
@@ -1,61 +1,72 @@
+#pragma once
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class Factura{
+class Factura {
 
-    private:
+private:
 
     string NumPieza, descrip;
     int cantiCompra, precio;
     double impuesto, descuento;
 
-    public:
+public:
 
     Factura(string Numpieza_, string descrip_, int cantiCompra_, int precio_, double impuesto_, double descuento_)
-        :NumPieza{Numpieza_}, descrip{descrip_}, cantiCompra{cantiCompra_}, precio{precio_}, impuesto{impuesto_}, descuento{descuento_}{}
+        :NumPieza{ Numpieza_ }, descrip{ descrip_ }, cantiCompra{ cantiCompra_ }, precio{ precio_ }, impuesto{ impuesto_ }, descuento{ descuento_ } {}
 
-    void setNum(string Numpieza_){
+    void setNum(string Numpieza_) {
         NumPieza = Numpieza_;
     }
-    void setDescri(string descrip_){
+    void setDescri(string descrip_) {
         descrip = descrip_;
     }
-    void setCanco(int cantiCompra_){
+    void setCanco(int cantiCompra_) {
         cantiCompra = cantiCompra_;
     }
-    void setPre(int precio_){
+    void setPre(int precio_) {
         precio = precio_;
     }
-    void setImpu(double impuesto_){
+    void setImpu(double impuesto_) {
         impuesto = impuesto_;
     }
-    void setDsc(double descuento_){
+    void setDsc(double descuento_) {
         descuento = descuento_;
     }
 
-    string getNumPieza(){
+    string getNumPieza() {
         return NumPieza;
     }
-    string getDescri(){
+    string getDescri() {
         return descrip;
     }
-    int getCanco(){
+    int getCanco() {
         return cantiCompra;
     }
-    int getPrecio(){
+    int getPrecio() {
         return precio;
     }
-    double getDsc(){
+    double getDsc() {
         return descuento;
     }
-    double getImpuesto(){
+    double getImpuesto() {
         return impuesto;
     }
 
-    double getInoviceAmount(){
-        return (cantiCompra * precio);
+    double getnvoiceAmount() {
+        double recibo = (cantiCompra * precio) + impuesto;
+        recibo = recibo - (descuento / 100);
+        return recibo;
     }
 
+    void display() {
+        cout << "La persona1: " << getNumPieza() << endl;
+        cout << "Descripcion: " << getDescri() << endl;
+        cout << "Cnatidad de objetos: " << getCanco() << endl;
+        cout << "Costo:  " << getPrecio() << endl;
+        cout << "Impuesto: " << getImpuesto() << endl;
+        cout << "Descuento: " << getDsc() << endl;
+    }
 };
