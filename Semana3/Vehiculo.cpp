1825675abc093f7cd0419362b4dd45399ca7a5ca
renatoqc <101827901+renatoqc@users.noreply.github.com>
2023-09-11 01:11:25 -0500

    Create Vehiculo.cpp

---
+++ Semana3/Vehiculo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Vehiculo.h"
+
+using namespace std;
+
+int main() {
+
+	
+
+	MotorVehicle carro{ "Mercedes-AMG GT 63 S E Performance", "gasolina", 2022, "negro", 1000 };
+	carro.getdisplayCarDetails();
+
+	return 0;
+}
