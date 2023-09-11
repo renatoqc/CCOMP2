7dd2cf24c1608b7ab25e9d4ca7b203cfbc522c33
renatoqc <101827901+renatoqc@users.noreply.github.com>
2023-09-11 01:12:34 -0500

    Update Factura.cpp

--- Semana3/Factura.cpp
+++ Semana3/Factura.cpp
@@ -1,9 +1,72 @@
+#pragma once
 #include <iostream>
-#include "Factura.h"
+#include <string>
 
 using namespace std;
 
-int main (){
+class Factura {
 
-    cout << ""
-}
+private:
+
+    string NumPieza, descrip;
+    int cantiCompra, precio;
+    double impuesto, descuento;
+
+public:
+
+    Factura(string Numpieza_, string descrip_, int cantiCompra_, int precio_, double impuesto_, double descuento_)
+        :NumPieza{ Numpieza_ }, descrip{ descrip_ }, cantiCompra{ cantiCompra_ }, precio{ precio_ }, impuesto{ impuesto_ }, descuento{ descuento_ } {}
+
+    void setNum(string Numpieza_) {
+        NumPieza = Numpieza_;
+    }
+    void setDescri(string descrip_) {
+        descrip = descrip_;
+    }
+    void setCanco(int cantiCompra_) {
+        cantiCompra = cantiCompra_;
+    }
+    void setPre(int precio_) {
+        precio = precio_;
+    }
+    void setImpu(double impuesto_) {
+        impuesto = impuesto_;
+    }
+    void setDsc(double descuento_) {
+        descuento = descuento_;
+    }
+
+    string getNumPieza() {
+        return NumPieza;
+    }
+    string getDescri() {
+        return descrip;
+    }
+    int getCanco() {
+        return cantiCompra;
+    }
+    int getPrecio() {
+        return precio;
+    }
+    double getDsc() {
+        return descuento;
+    }
+    double getImpuesto() {
+        return impuesto;
+    }
+
+    double getnvoiceAmount() {
+        double recibo = (cantiCompra * precio) + impuesto;
+        recibo = recibo - (descuento / 100);
+        return recibo;
+    }
+
+    void display() {
+        cout << "La persona1: " << getNumPieza() << endl;
+        cout << "Descripcion: " << getDescri() << endl;
+        cout << "Cnatidad de objetos: " << getCanco() << endl;
+        cout << "Costo:  " << getPrecio() << endl;
+        cout << "Impuesto: " << getImpuesto() << endl;
+        cout << "Descuento: " << getDsc() << endl;
+    }
+};
