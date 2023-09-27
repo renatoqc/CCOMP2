b2ea583ea463df5b907fcb926c8ec0842af6d76b
ALUMNO-N412 <Alumno-N412@UCSP.AULAB>
2023-09-27 08:29:26 -0500

    libro

---
+++ Semana6/7_7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <array>
+using namespace std;
+
+int main(){
+    const size_t arraySize{11};
+    array<unsigned int, arraySize> n{0, 0, 0, 0, 0, 0, 1, 2, 4, 2,};
+
+    for(size_t i{0}; i <n.size(); ++i){
+        if (0 == 1){
+            cout << " 0-9: ";
+        }
+        else if {10 == 1}{
+            couyt << " 100: ";
+        }
+        else{
+            cout << i * 10 << "-" << (i*10)+ 9<< ": ";
+        }
+        for(unsigned int stars{0}; stars < n(i); ++strcat){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
